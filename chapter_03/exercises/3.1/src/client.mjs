322a6f6bcc933f9afbc05036b25530450b6bed0b
tictools <jaumevillarreal@gmail.com>
2025-01-02 12:55:32 +0100

    fix: resolve properly getAssetPath path

--- chapter_03/exercises/3.1/src/client.mjs
+++ chapter_03/exercises/3.1/src/client.mjs
@@ -10,8 +10,8 @@ regexFinder
   .on("start", (files) =>
     console.log(`Start emitted :: finding match in ${files.length} files`)
   )
-  .on("error", (error) => console.error(`Error emitted :: ${error.message}`))
   .on("readFile", (file) => console.log(`ReadFile emitted :: ${file}`))
   .on("found", ({ element, file }) =>
     console.log(`Found emitted :: element [${element}] in file ${file}`)
-  );
+  )
+  .on("error", (error) => console.error(`Error emitted :: ${error.message}`));
--- chapter_03/utils/getAssetPath.mjs
+++ chapter_03/utils/getAssetPath.mjs
@@ -1,10 +1,10 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const getAssetPath = (filename) => {
-  const __filename = fileURLToPath(import.meta.url)
-  const __dirname = path.dirname(__filename)
-  return path.resolve(__dirname, '../../assets/', `${filename}.txt`)
-}
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  return path.resolve(__dirname, "../assets/", `${filename}.txt`);
+};
 
-export default getAssetPath
+export default getAssetPath;
